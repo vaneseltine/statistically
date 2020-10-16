3498a7b28ad48ba75591217da8f89a98966f2743
Matt VanEseltine <matvan@umich.edu>
2020-10-16 16:42:36 -0400

    Ease into real Line testing

--- statistically/lexer.py
+++ statistically/lexer.py
@@ -2,6 +2,34 @@ from pathlib import Path
 import re
 
 
+class LineLexer:
+    def __init__(self, text):  # , logger=None):
+        self.text = self.import_text(text)
+        self.outputs = []
+        self.lines = self.lex_lines(self.text)
+
+    @classmethod
+    def from_path(cls, path):
+        return cls(Path(path).read_text())
+
+    @staticmethod
+    def import_text(raw_text):
+        return [line.rstrip() for line in raw_text.splitlines()]
+
+    def lex_lines(self, text):
+        return {i: self.lex(s) for i, s in enumerate(text)}
+
+    @staticmethod
+    def lex(s):
+        token_class = LineToken.find(s)
+        token = token_class(s)
+        # print(f"{str(token):<20} {s}")
+        return token
+
+    def __len__(self):
+        return len(self.lines)
+
+
 class LineToken:
     priority = False
     include = []
@@ -19,20 +47,23 @@ class LineToken:
     def find(cls, s):
         checks = {subc: subc.match(s) for subc in cls.__subclasses__()}
         matches = [k for k, v in checks.items() if v]
-        if Command in matches:
-            # overrides other potential matches, since they could have anything
-            return Command
         try:
-            winner = cls.return_exactly_one(matches)
-        except ValueError as err:
-            print(s, matches)
-            raise (err)
+            return cls.ascertain_winning_match(matches)
+        except ValueError:
+            print(f"More than one match for {s}", *matches, sep="\n")
+
+    @classmethod
+    def ascertain_winning_match(cls, matches):
+        if Command in matches:
+            # overrides all others
+            return Command
+        winner = cls.exactly_one(matches)
         if winner is None:
             return Unknown
         return winner
 
     @staticmethod
-    def return_exactly_one(matches: set):
+    def exactly_one(matches: set):
         if not matches:
             return None
         if len(matches) > 1:
@@ -45,9 +76,9 @@ class LineToken:
 
     @classmethod
     def match(cls, s):
-        s = cls.prematch(s)
-        patterns_including = any(x.search(s) for x in cls.include)
-        patterns_excluding = any(x.search(s) for x in cls.exclude)
+        prepared = cls.prematch(s)
+        patterns_including = any(x.search(prepared) for x in cls.include)
+        patterns_excluding = any(x.search(prepared) for x in cls.exclude)
         is_match = patterns_including and not patterns_excluding
         return is_match
 
@@ -71,13 +102,13 @@ class Command(LineToken):
     ]
 
 
-class Blank(LineToken):
-    include = [r"^\s*$"]
-
-
 class Unknown(LineToken):
     def __str__(self):
         return "?"
+
+
+class Blank(LineToken):
+    include = [r"^\s*$"]
 
 
 class TableRow(LineToken):
@@ -97,33 +128,3 @@ class TableLineDiv(LineToken):
 
 class TableLineOuter(LineToken):
     include = [r"^\s{0,3}-+$"]
-
-
-class LineLexer:
-    def __init__(self, text):  # , logger=None):
-        self.text = self.import_text(text)
-        self.outputs = []
-        self.lines = self.lex_lines(self.text)
-
-    @staticmethod
-    def import_text(raw_text):
-        return [line.rstrip() for line in raw_text.splitlines()]
-
-    @classmethod
-    def from_path(cls, path):
-        return cls(Path(path).read_text())
-
-    def lex_lines(self, text):
-        return {i: self.lex(s) for i, s in enumerate(text)}
-
-    def lex(self, s):
-        token_class = LineToken.find(s)
-        token = token_class(s)
-        print(f"{str(token):<20} {s}")
-        return token
-
-    def __len__(self):
-        return len(self.lines)
-
-    def __str__(self):
-        return f'{self.__class__.__name__}("something or other")'
--- test/test_lexer.py
+++ test/test_lexer.py
@@ -1,27 +1,48 @@
+import sys
 from pathlib import Path
 
 import pytest
 
-from statistically.lexer import LineLexer
+from statistically.lexer import LineLexer, LineToken, TableRow
 
 EXAMPLE_DIR = Path(__file__).parent / "examples"
 
 
 class TestLineLexerBasics:
-    # l = LineLexer.from_path(EXAMPLE_DIR / "tebalance.txt")
-    l = LineLexer.from_path(EXAMPLE_DIR / "multiple" / "full_log.txt")
-
-    def t_lex(self):
-        assert len(self.l) == len(self.l.text)
+    def t_lex_keeps_all_lines(self):
+        l = LineLexer.from_path(EXAMPLE_DIR / "multiple" / "full_log.txt")
+        assert len(l) == len(l.text)
 
 
-colnames = [
-    "    Variable |        Obs        Mean    Std. Dev.       Min        Max",
-    "   Group |     Obs        Mean    Std. Err.   Std. Dev.   [95% Conf. Interval]",
-    "               two_n_pubs |      Coef.   Std. Err.      z    P>|z|     [95% Conf. Interval]",
-    "                  |Standardized differences          Variance ratio",
-    "                  |        Raw     Matched           Raw    Matched",
-]
+class TestTableRow:
+    @staticmethod
+    @pytest.mark.parametrize(
+        "line",
+        [
+            "    Variable |        Obs        Mean    Std. Dev.       Min        Max",
+            "   Group |     Obs        Mean    Std. Err.   Std. Dev.   [95% Conf. Interval]",
+            "               two_n_pubs |      Coef.   Std. Err.      z    P>|z|     [95% Conf. Interval]",
+            "                  |Standardized differences          Variance ratio",
+            "                  |        Raw     Matched           Raw    Matched",
+        ],
+    )
+    def t_good(line):
+        assert LineToken.find(line) == TableRow
+        assert LineLexer.lex(line).__class__ == TableRow
 
-l = LineLexer.from_path(EXAMPLE_DIR / "nbreg.txt")
-print(*l.lines.items(), sep="\n")
+    @staticmethod
+    @pytest.mark.parametrize(
+        "line",
+        [
+            "                          Raw     Matched           Raw    Matched",
+            " Pr(T < t) = 1.0000         Pr(|T| > |t|) = 0.0000          Pr(T > t) = 0.0000",
+        ],
+    )
+    def t_bad(line):
+        assert LineToken.find(line) != TableRow
+        assert LineLexer.lex(line).__class__ != TableRow
+
+
+if __name__ == "__main__" and sys.argv[1:]:
+    lexed = LineLexer.from_path(" ".join(sys.argv[1:]))
+    print(*lexed.lines.items(), sep="\n")
